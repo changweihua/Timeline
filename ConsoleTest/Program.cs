bc03869ab442c2500abd30907f978fbd4f43cb2e
Changweihua <changweihua@outlook.com>
2013-11-26 16:03:04 +0800

    小更新

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -7,8 +7,22 @@ namespace ConsoleTest
 {
     class Program
     {
+        const int Width = 1920;
+
         static void Main(string[] args)
         {
+            for (int i = 0; i < 12; i++)
+            {
+
+            }
+
+            Console.ReadKey(true);
         }
+
+        static void Calc(int count)
+        { 
+            
+        }
+
     }
 }
